e56141e77da73dc773e7a77ca1599c81b4aea2ba
Wiseman Mabusela <95911962+Wiseman930@users.noreply.github.com>
2022-07-20 10:03:01 +0200

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let app = express();
 
+
 app.get("/", function(req, res){
   res.send("Bill Settings WebApp");
 });
@@ -10,3 +11,7 @@ let PORT = process.env.PORT || 3007;
 app.listen(PORT, function(){
   console.log('App starting on port', PORT);
 });
+
+const exphbs  = require('express-handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');

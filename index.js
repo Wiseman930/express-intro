290cc97e91dea3dcd570a3504799c81859586f02
Wiseman Mabusela <95911962+Wiseman930@users.noreply.github.com>
2022-07-25 12:18:04 +0200

    update

--- index.js
+++ index.js
@@ -3,13 +3,22 @@ let app = express();
 
 
 app.get("/", function(req, res){
-  res.send("Wiseman");
+  res.send("Wiseman Mabusela");
 });
+
+
+const exphbs  = require('express-handlebars');
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+  res.render('home');
+});
+
 
 let PORT = process.env.PORT || 3007;
 
 app.listen(PORT, function(){
   console.log('App starting on port', PORT);
 });
-
 
